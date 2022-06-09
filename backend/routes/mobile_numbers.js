a5b4b6875ac6c9a567bb658f0d551c2f13a4ec10
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-06-09 17:56:01 +0200

    called controllers for router

--- backend/routes/mobile_numbers.js
+++ backend/routes/mobile_numbers.js
@@ -2,6 +2,14 @@ import express from "express";
 
 const router = express.Router();
 
+import {
+  getAllNumbers,
+  getNumber,
+  createNumber,
+  deleteNumber,
+  updateNumber,
+} from "../controllers/mobile_numbers.js";
+
 router.route("/").get(getAllNumbers).post(createNumber);
 router.route("/:id").delete(deleteNumber).patch(updateNumber).get(getNumber);
 
