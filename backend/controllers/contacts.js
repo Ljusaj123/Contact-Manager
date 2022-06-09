eaf2f631e9264c4aa22911b72a4b018edd7d0a32
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-06-09 18:31:44 +0200

    created CRUD operations for contacts

--- backend/controllers/contacts.js
+++ backend/controllers/contacts.js
@@ -1,15 +1,58 @@
-import mongoose from "mongoose";
+import Contact from "../models/Contact.js";
+import { StatusCodes } from "http-status-codes";
 
-export const getAllContacts = async (req, res, next) => {};
+export const getAllContacts = async (req, res, next) => {
+  const allContacts = await Contact.find({});
+  res.status(StatusCodes.OK).json({ allContacts });
+};
 export const getContact = async (req, res, next) => {
-  console.log("Single Number");
+  try {
+    const { id: contactID } = req.params;
+
+    const contact = await Contact.findById(contactID);
+    if (!contact) {
+      throw new Error(`contact does not exist with id ${contactID}`);
+    }
+    res.status(StatusCodes.OK).json({ contact });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
 export const deleteContact = async (req, res, next) => {
-  console.log("delete number");
+  try {
+    const { id: contactID } = req.params;
+
+    const contact = await Contact.findByIdAndRemove(contactID);
+    if (!contact) {
+      throw new Error(`contact does not exist with id ${contactID}`);
+    }
+    res.status(StatusCodes.OK).json(`contact with id ${contactID} is deleted`);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
 export const updateContact = async (req, res, next) => {
-  console.log("All numbers");
+  try {
+    const { id: contactID } = req.params;
+    const contact = await Contact.findByIdAndUpdate(
+      contactID,
+      { $set: req.body },
+      { new: true, runValidators: true }
+    );
+    if (!contact) {
+      throw new Error(`contact does not exist with id ${contactID}`);
+    }
+    res.status(StatusCodes.OK).json({ contact });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
 export const createContact = async (req, res, next) => {
-  console.log("All numbers");
+  try {
+    const newContact = Contact(req.body);
+    const contact = await newContact.save();
+    res.status(StatusCodes.OK).json({ contact });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
