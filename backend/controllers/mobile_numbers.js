3c2e45ea05baefab4aae327e4c50aad42e330974
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-06-09 17:55:45 +0200

    created controllers for numbers

---
+++ backend/controllers/mobile_numbers.js
@@ -0,0 +1,15 @@
+export const getAllNumbers = async (req, res, next) => {
+  console.log("All numbers");
+};
+export const getNumber = async (req, res, next) => {
+  console.log("Single Number");
+};
+export const deleteNumber = async (req, res, next) => {
+  console.log("delete number");
+};
+export const updateNumber = async (req, res, next) => {
+  console.log("All numbers");
+};
+export const createNumber = async (req, res, next) => {
+  console.log("All numbers");
+};
