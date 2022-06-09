59de084311b9e8bb4d162e189f2d31568db28400
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-06-09 18:49:26 +0200

    called middlewares

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,17 @@ dotenv.config();
 const app = express();
 import connectDB from "./database/connect.js";
 import contactRoute from "./routes/contacts.js";
+import ErrorHandlerMiddleware from "./middlewares/ErrorHandlerMiddleware.js";
+import NotFoundMiddleware from "./middlewares/NotFoundMiddleware.js";
 
-app.use(express.json());
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
+app.use(express.json());
 app.use("/api/v1/contacts", contactRoute);
+app.use(ErrorHandlerMiddleware);
+app.use(NotFoundMiddleware);
 
 const start = async () => {
   await connectDB(process.env.MONGO_URI);
