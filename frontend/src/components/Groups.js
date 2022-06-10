63a97f0e9be09a469d4ff8a442702d4dea16bb23
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-06-11 00:03:18 +0200

    created component for groups and styles

---
+++ frontend/src/components/Groups.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BiStar, BiGroup, BiUser } from "react-icons/bi";
+
+function Nesto() {
+  return (
+    <div className="groups">
+      <div className="groups-container">
+        <BiUser />
+        <BiStar />
+        <BiGroup />
+      </div>
+    </div>
+  );
+}
+
+export default Nesto;
