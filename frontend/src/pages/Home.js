0f951cc227959eba2dcecb551ef5d2d3dee901b2
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-06-10 18:51:34 +0200

    called contact list

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
-import Header from "../components/Header";
+import ContactList from "../components/ContactList";
+import Header from "../layout/Header";
 
 function Home() {
   return (
     <div className="home">
       <div className="home-container">
         <Header />
+        <ContactList />
       </div>
     </div>
   );
