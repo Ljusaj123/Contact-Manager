ce5c7a2e4b502a2bbc0376be78cdb249970381fe
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-06-10 23:49:51 +0200

    added icon for create user

--- frontend/src/layout/Header.js
+++ frontend/src/layout/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { BiUserPlus } from "react-icons/bi";
 
 function Header() {
   return (
     <header>
       <div className="header-container">
+        <BiUserPlus />
         <h1>Contact Manager</h1>
         <input type="text" placeholder="Search" />
       </div>
