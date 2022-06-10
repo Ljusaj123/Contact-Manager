3948e9f57fce8d35277b4976d4db8fa78caf7072
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-06-10 23:48:38 +0200

    style for home component

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ContactList from "../components/ContactList";
+import Groups from "../components/Groups";
 import Header from "../layout/Header";
 
 function Home() {
   return (
     <div className="home">
       <div className="home-container">
-        <Header />
-        <ContactList />
+        <div className="container">
+          <Header />
+          <Groups />
+          <ContactList />
+        </div>
       </div>
     </div>
   );
