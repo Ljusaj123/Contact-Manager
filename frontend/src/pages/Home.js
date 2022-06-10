2fd7f5a2670565c912e301cb288f51d77836af31
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-06-10 18:30:15 +0200

    created pages

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home-container">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
