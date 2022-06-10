42c10f67b1a866606c1243bbf577fcb5596e1a1f
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-06-10 18:51:14 +0200

    new folder for layout components

---
+++ frontend/src/layout/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div className="header-container">
+        <h1>Contact Manager</h1>
+        <input type="text" placeholder="Search" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
