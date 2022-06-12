76d699788cc815fe22d3c72d66fd89c39ae8e645
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-06-12 14:06:30 +0200

    mapping the array of contacts

--- frontend/src/components/ContactList.js
+++ frontend/src/components/ContactList.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Contact from "./Contact";
+import { Link } from "react-router-dom";
+import { niz } from "../niz.js";
+
 function ContactList() {
   return (
     <div className="contact-list">
       <div className="contact-list-container">
-        <Contact />
-        <Contact />
-        <Contact />
-        <Contact />
+        {niz.map((user) => {
+          return (
+            <Link to={`/contact/${user.id}`} key={user.id}>
+              <Contact props={user} />
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
