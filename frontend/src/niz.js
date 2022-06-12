dbd8c08f827a5b354bca2d37c694b6ff4ff3ab71
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-06-12 14:04:29 +0200

    for example array of contacts

---
+++ frontend/src/niz.js
@@ -0,0 +1,29 @@
+export const niz = [
+  {
+    id: "789",
+    name: "Anna",
+    surname: "Marija",
+    number: ["73498739486347", "3845934789347"],
+    email: ["ana@gmail.com", "anna45@gmail.com"],
+    address: "Somewhere",
+    tag: "Friend",
+  },
+  {
+    id: "123",
+    name: "Ante",
+    surname: "Marijo",
+    number: [734987345347, 3845934789347],
+    email: ["luka@gmail.com", "luka45@gmail.com"],
+    address: "Somewhere",
+    tag: "",
+  },
+  {
+    id: "456",
+    name: "Sara",
+    surname: "Ljusaj",
+    number: [345498739486347, 3845934789347],
+    email: ["sara@gmail.com", "sara45@gmail.com"],
+    address: "Somewhere",
+    tag: "Family",
+  },
+];
