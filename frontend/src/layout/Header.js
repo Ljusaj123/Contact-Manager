4258314e50b2b5b1c6d95da5e0b7305b4213deed
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-06-12 14:03:49 +0200

    state for handling query

--- frontend/src/layout/Header.js
+++ frontend/src/layout/Header.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { BiUserPlus } from "react-icons/bi";
+import { useState } from "react";
 
 function Header() {
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setQuery("");
+  };
   return (
     <header>
-      <div className="header-container">
-        <BiUserPlus className="new-user-icon" />
-        <h1>Contact Manager</h1>
-        <input type="text" placeholder="Search" />
-      </div>
+      <BiUserPlus className="create-user-icon" />
+      <h1>Contact Manager</h1>
+      <form action="#" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </form>
     </header>
   );
 }
