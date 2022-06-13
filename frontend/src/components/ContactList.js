93d1a1057dd4770f9a88b164db29e55a97a2717a
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-06-13 02:09:46 +0200

    custom hook for fetching data and hanling error or loading

--- frontend/src/components/ContactList.js
+++ frontend/src/components/ContactList.js
@@ -1,16 +1,29 @@
 import React from "react";
 import Contact from "./Contact";
 import { Link } from "react-router-dom";
-import { niz } from "../niz.js";
+import useFetch from "../hooks/useFetch.js";
 
 function ContactList() {
+  const { data, isLoading, error } = useFetch(
+    "http://localhost:6001/api/v1/contacts"
+  );
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error....</p>;
+  }
+
   return (
     <div className="contact-list">
       <div className="contact-list-container">
-        {niz.map((user) => {
+        {data.map((user) => {
+          const { name, _id: id } = user;
           return (
-            <Link to={`/contact/${user.id}`} key={user.id}>
-              <Contact props={user} />
+            <Link to={`/contacts/${id}`} key={id}>
+              <Contact name={name} />
             </Link>
           );
         })}
