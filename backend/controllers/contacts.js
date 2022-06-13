3dd79068f73a175037a9371ade89f9a6adc9bd77
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-06-13 02:07:46 +0200

    changed name of response

--- backend/controllers/contacts.js
+++ backend/controllers/contacts.js
@@ -3,18 +3,18 @@ import { StatusCodes } from "http-status-codes";
 import { BadRequest } from "../errors/CustomErrors.js";
 
 export const getAllContacts = async (req, res, next) => {
-  const allContacts = await Contact.find({});
-  res.status(StatusCodes.OK).json({ allContacts });
+  const contacts = await Contact.find({});
+  res.status(StatusCodes.OK).json({ contacts });
 };
 export const getContact = async (req, res, next) => {
   try {
     const { id: contactID } = req.params;
 
-    const contact = await Contact.findById(contactID);
-    if (!contact) {
+    const contacts = await Contact.findById(contactID);
+    if (!contacts) {
       throw new BadRequest(`contact does not exist with id ${contactID}`);
     }
-    res.status(StatusCodes.OK).json({ contact });
+    res.status(StatusCodes.OK).json({ contacts });
   } catch (error) {
     next(error);
   }
@@ -23,8 +23,8 @@ export const deleteContact = async (req, res, next) => {
   try {
     const { id: contactID } = req.params;
 
-    const contact = await Contact.findByIdAndRemove(contactID);
-    if (!contact) {
+    const contacts = await Contact.findByIdAndRemove(contactID);
+    if (!contacts) {
       throw new BadRequest(`contact does not exist with id ${contactID}`);
     }
     res.status(StatusCodes.OK).json(`contact with id ${contactID} is deleted`);
@@ -35,15 +35,15 @@ export const deleteContact = async (req, res, next) => {
 export const updateContact = async (req, res, next) => {
   try {
     const { id: contactID } = req.params;
-    const contact = await Contact.findByIdAndUpdate(
+    const contacts = await Contact.findByIdAndUpdate(
       contactID,
       { $set: req.body },
       { new: true, runValidators: true }
     );
-    if (!contact) {
+    if (!contacts) {
       throw new BadRequest(`contact does not exist with id ${contactID}`);
     }
-    res.status(StatusCodes.OK).json({ contact });
+    res.status(StatusCodes.OK).json({ contacts });
   } catch (error) {
     next(error);
   }
@@ -51,8 +51,8 @@ export const updateContact = async (req, res, next) => {
 export const createContact = async (req, res, next) => {
   try {
     const newContact = Contact(req.body);
-    const contact = await newContact.save();
-    res.status(StatusCodes.CREATED).json({ contact });
+    const contacts = await newContact.save();
+    res.status(StatusCodes.CREATED).json({ contacts });
   } catch (error) {
     next(error);
   }
