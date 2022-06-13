a8f638c4e0c72818236b9e7470b8f5bb07d9fc8a
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-06-13 02:10:05 +0200

    custom hook for fetching data and hanling error or loading

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -3,68 +3,54 @@ import { AiOutlineDelete } from "react-icons/ai";
 import { MdFavoriteBorder } from "react-icons/md";
 import { BsPencil } from "react-icons/bs";
 import { FaUserCircle } from "react-icons/fa";
-import { niz } from "../niz.js";
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch.js";
 
 function Contact() {
   const { id } = useParams();
 
-  const [user, setUser] = useState([]);
+  const {
+    data: user,
+    isLoading,
+    error,
+  } = useFetch(`http://localhost:6001/api/v1/contacts/${id}`);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
-  useEffect(() => {
-    const us = niz.find((a) => a.id === id);
-    setUser([us]);
-  }, [id]);
+  if (error) {
+    return <p>Error....</p>;
+  }
+
+  const { name, email, number, address, tag } = user;
 
   return (
     <div className="contact-page">
-      {console.log(user)}
       <div className="contact-page-container">
         <div className="container">
           <h1>Contact Info</h1>
           <div className="image-name-container">
             <FaUserCircle className="user-info-icon" />
-            <h2>{user.name}</h2>
+            <h2>{name}</h2>
           </div>
           <div className="number-email-container">
             <div className="email-container">
               <h3>Email:</h3>
-              {user.map((us) => {
-                return (
-                  <div className="emails">
-                    {us.email.map((u, index) => {
-                      return <p key={index}>{u}</p>;
-                    })}
-                  </div>
-                );
-              })}
+              <p>{email}</p>
             </div>
             <div className="number-container">
               <h3>Number:</h3>
-              {user.map((us) => {
-                return (
-                  <div className="numbers">
-                    {us.number.map((u, index) => {
-                      return <p key={index}>{u}</p>;
-                    })}
-                  </div>
-                );
-              })}
+              <p>{number}</p>
             </div>
           </div>
           <div className="rest-info">
             <div className="address-container">
               <h3>Address:</h3>
-              {user.map((us, index) => {
-                return <p key={index}>{us.address}</p>;
-              })}
+              <p>{address}</p>
             </div>
             <div className="tag-container">
               <h3>Tag:</h3>
-              {user.map((us, index) => {
-                return <p key={index}>{us.tag}</p>;
-              })}
+              <p>{tag ? tag : "No tag"}</p>
             </div>
           </div>
           <div className="button-container">
