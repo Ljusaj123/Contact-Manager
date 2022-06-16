32f4e99b8e2539dd27bd822ef2f6e900450de40a
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-06-16 21:16:38 +0200

    changed class names

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
-    <div className="not-found">
-      <h1>Error 404 - Page not found</h1>
-      <h3>The page you requested does not exist</h3>
-      <Link to="/">
-        <button className="back-btn">Back to Homepage</button>
-      </Link>
+    <div className="error-page">
+      <div className="error-page-container">
+        <h1>Error 404 - Page not found</h1>
+        <h3>The page you requested does not exist</h3>
+        <Link to="/">
+          <button className="back-btn">Back to Homepage</button>
+        </Link>
+      </div>
     </div>
   );
 }
