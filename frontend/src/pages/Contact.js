086e2a74e94445127ae770e3603f1259670a1383
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-06-16 21:17:44 +0200

    spinner and error page in conditional rendering, mapping through emails and numbers, different class names

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -5,6 +5,9 @@ import { BsPencil } from "react-icons/bs";
 import { FaUserCircle } from "react-icons/fa";
 import { useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch.js";
+import ErrorPage from "./ErrorPage.js";
+import "react-spinner-animated/dist/index.css";
+import { HalfMalf } from "react-spinner-animated";
 
 function Contact() {
   const { id } = useParams();
@@ -15,43 +18,56 @@ function Contact() {
     error,
   } = useFetch(`http://localhost:6001/api/v1/contacts/${id}`);
   if (isLoading) {
-    return <p>Loading...</p>;
+    return (
+      <HalfMalf
+        text={"Loading..."}
+        bgColor={"#393e4b"}
+        width={"150px"}
+        height={"150px"}
+      />
+    );
   }
 
   if (error) {
-    return <p>Error....</p>;
+    return <ErrorPage error={error} />;
   }
 
   const { name, email, number, address, tag } = user;
 
   return (
     <div className="contact-page">
+      {/* {console.log(user)} */}
       <div className="contact-page-container">
         <div className="container">
           <h1>Contact Info</h1>
-          <div className="image-name-container">
+          <div className="image-name-container ">
             <FaUserCircle className="user-info-icon" />
             <h2>{name}</h2>
           </div>
-          <div className="number-email-container">
-            <div className="email-container">
-              <h3>Email:</h3>
-              <p>{email}</p>
-            </div>
-            <div className="number-container">
-              <h3>Number:</h3>
-              <p>{number}</p>
+
+          <div className="email">
+            <h3>Email:</h3>
+            <div className="number-paragraphs">
+              {(email || []).map((mail, index) => {
+                return <p key={index}>{mail}</p>;
+              })}
             </div>
           </div>
-          <div className="rest-info">
-            <div className="address-container">
-              <h3>Address:</h3>
-              <p>{address}</p>
+          <div className="number">
+            <h3>Number:</h3>
+            <div className="number-paragraphs">
+              {(number || []).map((numb, index) => {
+                return <p key={index}>{numb}</p>;
+              })}
             </div>
-            <div className="tag-container">
-              <h3>Tag:</h3>
-              <p>{tag ? tag : "No tag"}</p>
-            </div>
+          </div>
+          <div className="address">
+            <h3>Address:</h3>
+            <p>{address}</p>
+          </div>
+          <div className="tag">
+            <h3>Tag:</h3>
+            <p>{tag ? tag : "No tag"}</p>
           </div>
           <div className="button-container">
             <AiOutlineDelete className="button-icon" />
